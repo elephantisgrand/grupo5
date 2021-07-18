12b4b69f3d2184a3e508bc77ca38676ac769bf59
elphantisgrand <oswaldosantillan26@gmail.com>
2021-07-18 10:49:53 -0500

    Html cabecera, menu de navegacion

---
+++ index.php
@@ -0,0 +1,39 @@
+<!Doctype>
+<html lang="es">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Blog de programacion</title>
+  </head>
+  <body>
+    <!--CABECERA-->
+    <header id="header">
+      <!--logo-->
+      <div id ="logo">
+	<a>Blog programacion git</a>
+      </div>
+
+    <!--MENU-->
+    <nav id="nav">
+      <ul>
+	<li><a href="index.php">Inicio</a></li>
+	<li><a href="index.php">Lenguaje 1</a></li>
+	<li><a href="index.php">Lenguaje 2</a></li>
+	<li><a href="index.php">Lenguaje 3</a></li>
+	<li><a href="index.php">Lenguaje 4</a></li>
+	<li><a href="index.php">Contacto</a></li>
+      </ul>
+    </nav>
+    
+    </header>
+
+
+    <!--BARRA LATERAL-->
+
+
+    <!--CAJA PRINCIPAL-->
+
+    
+    <!--PIE DE PAGINA-->
+  </body>
+
+</html>
